ad54fade14ea5341dc0435af2972a893af96a52e
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-16 13:25:41 +0530

    client connected to server sucessfully

--- client/Program.cs
+++ client/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50051";//the server address
+        static void Main(string[] args)
+        {
+           Channel channel = new Channel(target, ChannelCredentials.Insecure);//create a channel
+
+           channel.ConnectAsync().ContinueWith((task) =>//connect to the server
+           {
+               if (task.Status == TaskStatus.RanToCompletion)
+               {
+                   Console.WriteLine("The client connected successfully");
+               }
+               else
+               {
+                   Console.WriteLine("Failed to connect to the server");
+               }
+                   
+           });
+
+            var client = new Dummy.DummyService.DummyServiceClient(channel);//create a client
+
+            channel.ShutdownAsync().Wait();//wait for the channel to shutdown
+            Console.ReadKey();//wait for the user to press a key
+        }
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{     class Program
+    {
+        const int Port = 50051;
+        static void Main(string[] args)
+        {
+            Server server = null;
+            try
+            {
+                server = new Server
+                {
+                    Ports = { new ServerPort("127.0.0.1", Port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine("Server listening on port " + Port);
+                Console.ReadKey();
+            }
+            catch(IOException e)
+            { 
+                Console.WriteLine("Server failed to start: " + e.Message);
+                throw new Exception("Server failed to start", e);
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
+        }
+    }
+}
