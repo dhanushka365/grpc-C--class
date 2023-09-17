102efc032fcfbe6fa9267d5e0a103536d9273417
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-17 06:57:58 +0530

    Server streaming API Server Implementation Completed

--- server/GreetingServiceImpl.cs
+++ server/GreetingServiceImpl.cs
@@ -15,5 +15,17 @@ namespace server
         {
             return Task.FromResult(new GreetingResponse() { Result = "Hello " + request.Greeting.FirstName + " " + request.Greeting.LastName });
         }
+
+        public override async Task GreetManyTimes(GreetManyTimesRequest request, IServerStreamWriter<GreetManyTimesResponse> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine("The server received the request: " + request.ToString());
+            string result = "Hello " + request.Greeting.FirstName + " " + request.Greeting.LastName;
+            for (int i = 0; i < 10; i++)
+            {
+                GreetManyTimesResponse response = new GreetManyTimesResponse() { Result = result };
+                await responseStream.WriteAsync(response);
+            }
+            //return base.GreetManyTimes(request, responseStream, context);
+        }
     }
 }
