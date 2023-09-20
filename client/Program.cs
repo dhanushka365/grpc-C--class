fa665b58abe3712a1b478897e0f0aaf55bd656bc
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-20 15:24:57 +0530

    works commited to github

--- client/Program.cs
+++ client/Program.cs
@@ -10,31 +10,47 @@ namespace client
 {
     class Program
     {
-        const string target = "127.0.0.1:50051";//the server address
-        static void Main(string[] args)
+        const string target = "127.0.0.1:50051"; // the server address
+
+        static async Task Main(string[] args)
         {
-           Channel channel = new Channel(target, ChannelCredentials.Insecure);//create a channel
+            Channel channel = new Channel(target, ChannelCredentials.Insecure); // create a channel
 
-           channel.ConnectAsync().ContinueWith((task) =>//connect to the server
-           {
-               if (task.Status == TaskStatus.RanToCompletion)
-               {
-                   Console.WriteLine("The client connected successfully");
-               }
-               else
-               {
-                   Console.WriteLine("Failed to connect to the server");
-               }
-                   
-           });
+            await channel.ConnectAsync().ContinueWith((task) => // connect to the server
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The client connected successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to connect to the server");
+                }
 
-            //var client = new Dummy.DummyService.DummyServiceClient(channel);//create a client
-            var client = new GreetingService.GreetingServiceClient(channel);//create a client
+            });
 
-            var response = client.Greet(new GreetingRequest() { Greeting = new Greeting() { FirstName = "John", LastName = "Doe" } });//send a request to the server
-            Console.WriteLine(response.Result);//print the response
-            channel.ShutdownAsync().Wait();//wait for the channel to shutdown
-            Console.ReadKey();//wait for the user to press a key
+            var client = new GreetingService.GreetingServiceClient(channel); // create a client
+            var greeting = new Greeting()
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            }; // create a greeting
+
+            var request = new LongGreetRequest() { Greeting = greeting }; // create a request
+            var stream = client.LongGreet(); // create a response
+
+            foreach (int i in Enumerable.Range(1, 10))
+            {
+                // Console.WriteLine("Sending stream");
+                await stream.RequestStream.WriteAsync(request); // send the request
+            }
+            await stream.RequestStream.CompleteAsync();
+
+            var response = await stream.ResponseAsync;
+            Console.WriteLine(response.Result); // print the response
+
+            await channel.ShutdownAsync(); // wait for the channel to shutdown
+            Console.ReadKey(); // wait for the user to press a key
         }
     }
 }
